2f62d5787277548d68e9fb4974791acadfec08e5
likewindwang <ityanqing@163.com>
2018-08-20 17:11:13 +0800

    更改layui弹窗

--- resource/js/company/check-layer.js
+++ resource/js/company/check-layer.js
@@ -1,3 +1,33 @@
 $(function(){
-    console.log(location.href)
+    var sectionData = ['经理部','市场部','运营部','产品部','研发部']
+    // layui日期组件的调用
+    layui.use(['form','laydate'], function(){
+        var form = layui.form,
+            laydate = layui.laydate,
+            table = layui.table;
+
+        laydate.render({
+            elem: '#test1-1'
+            ,lang: 'en'
+        })
+        laydate.render({
+            elem: '#test1-2'
+            ,lang: 'en'
+        })
+    })
+    // 在列表点击后传过来的值
+    var getParams = JSON.parse(decodeURI(window.location.search.split('?')[1]))
+    // 将穿过来的值渲染至DOM
+    for(var k in getParams){
+        $('.person-'+k+'').val(getParams[k])
+        // if(k == 'section'){
+        //     $('.person-'+k+'').val(getParams[k])
+        // }else{
+        //     $('.person-'+k+'').val(getParams[k])
+        // }
+    }
+    // 下拉选择（部门）
+
+
+
 })
--- resource/js/company/workers.js
+++ resource/js/company/workers.js
@@ -31,43 +31,43 @@ $(function(){
         var table = layui.table;
         table.render({
           elem: '#test'
-          ,height:'full-200'
+          ,height:'full-140' // 差值，充满，并且距离底部的距离
           ,skin:'line'
-          ,data:[{name:'1',sex:'男',city:'2015',sign:'未填写',id:'1000',score:'2017-08-25 16:06'},
-              {name:'2',sex:'女',city:'2016',sign:'未填写',id:'1001',score:'2017-08-25 16:06'},
-              {name:'3',sex:'男',city:'2017',sign:'未填写',id:'1002',score:'2017-08-25 16:06'},
-              {name:'4',sex:'男',city:'2018',sign:'未填写',id:'1003',score:'2017-08-25 16:06'},
-              {name:'5',sex:'男',city:'2019',sign:'未填写',id:'1004',score:'2017-08-25 16:06'},
-              {name:'6',sex:'女',city:'2020',sign:'未填写',id:'1005',score:'2017-08-25 16:06'},
-              {name:'7',sex:'女',city:'2021',sign:'未填写',id:'1006',score:'2017-08-25 16:06'},
-              {name:'8',sex:'女',city:'2022',sign:'未填写',id:'1007',score:'2017-08-25 16:06'},
-              {name:'9',sex:'女',city:'2023',sign:'未填写',id:'1008',score:'2017-08-25 16:06'},
-              {name:'10',sex:'女',city:'2015',sign:'未填写',id:'1009',score:'2017-08-25 16:06'},
-              {name:'7',sex:'女',city:'2021',sign:'未填写',id:'1006',score:'2017-08-25 16:06'},
-              {name:'8',sex:'女',city:'2022',sign:'未填写',id:'1007',score:'2017-08-25 16:06'},
-              {name:'9',sex:'女',city:'2023',sign:'未填写',id:'1008',score:'2017-08-25 16:06'},
-              {name:'10',sex:'女',city:'2015',sign:'未填写',id:'1009',score:'2017-08-25 16:06'},
-              {name:'7',sex:'女',city:'2021',sign:'未填写',id:'1006',score:'2017-08-25 16:06'},
-              {name:'8',sex:'女',city:'2022',sign:'未填写',id:'1007',score:'2017-08-25 16:06'},
-              {name:'9',sex:'女',city:'2023',sign:'未填写',id:'1008',score:'2017-08-25 16:06'},
-              {name:'10',sex:'女',city:'2015',sign:'未填写',id:'1009',score:'2017-08-25 16:06'},
-              {name:'7',sex:'女',city:'2021',sign:'未填写',id:'1006',score:'2017-08-25 16:06'},
-              {name:'8',sex:'女',city:'2022',sign:'未填写',id:'1007',score:'2017-08-25 16:06'},
-              {name:'9',sex:'女',city:'2023',sign:'未填写',id:'1008',score:'2017-08-25 16:06'},
-              {name:'10',sex:'女',city:'2015',sign:'未填写',id:'1009',score:'2017-08-25 16:06'}
+          ,data:[{name:'赵老大',sex:'男',section:'2015',inTime:'2010-03-21',id:'1000',outTime:'2017-08-25'},
+              {name:'赵小二',sex:'女',section:'2016',inTime:'2010-03-21',id:'1001',outTime:'2017-08-25'},
+              {name:'赵三儿',sex:'男',section:'2017',inTime:'2010-03-21',id:'1002',outTime:'2017-08-25'},
+              {name:'钱进',sex:'男',section:'2018',inTime:'2010-03-21',id:'1003',outTime:'2017-08-25'},
+              {name:'周油',sex:'男',section:'2019',inTime:'2010-03-21',id:'1004',outTime:'2017-08-25'},
+              {name:'吴凯',sex:'女',section:'2020',inTime:'2010-03-21',id:'1005',outTime:'2017-08-25'},
+              {name:'张飞',sex:'女',section:'2021',inTime:'2010-03-21',id:'1006',outTime:'2017-08-25'},
+              {name:'张懿德',sex:'女',section:'2022',inTime:'2010-03-21',id:'1007',outTime:'2017-08-25'},
+              {name:'司马老儿',sex:'女',section:'2023',inTime:'2010-03-21',id:'1008',outTime:'2017-08-25'},
+              {name:'孔明',sex:'女',section:'2015',inTime:'2010-03-21',id:'1009',outTime:'2017-08-25'},
+              {name:'卧龙',sex:'女',section:'2021',inTime:'2010-03-21',id:'1006',outTime:'2017-08-25'},
+              {name:'飞将军',sex:'女',section:'2022',inTime:'2010-03-21',id:'1007',outTime:'2017-08-25'},
+              {name:'蒙毅',sex:'女',section:'2023',inTime:'2010-03-21',id:'1008',outTime:'2017-08-25'},
+              {name:'伙头军',sex:'女',section:'2015',inTime:'2010-03-21',id:'1009',outTime:'2017-08-25'},
+              {name:'齐天大圣',sex:'女',section:'2021',inTime:'2010-03-21',id:'1006',outTime:'2017-08-25'},
+              {name:'方天画戟',sex:'女',section:'2022',inTime:'2010-03-21',id:'1007',outTime:'2017-08-25'},
+              {name:'丈八蛇矛',sex:'女',section:'2023',inTime:'2010-03-21',id:'1008',outTime:'2017-08-25'},
+              {name:'长枪依在',sex:'女',section:'2015',inTime:'2010-03-21',id:'1009',outTime:'2017-08-25'},
+              {name:'大杀四方',sex:'女',section:'2021',inTime:'2010-03-21',id:'1006',outTime:'2017-08-25'},
+              {name:'断头台',sex:'女',section:'2022',inTime:'2010-03-21',id:'1007',outTime:'2017-08-25'},
+              {name:'天音波',sex:'女',section:'2023',inTime:'2010-03-21',id:'1008',outTime:'2017-08-25'},
+              {name:'狂风绝息斩',sex:'女',section:'2015',inTime:'2010-03-21',id:'1009',outTime:'2017-08-25'}
           ]
           ,cols: [[
             {width:'6%', type:'checkbox'}
             ,{field:'id', width:'10%', title: 'ID'}
             ,{field:'name', width:'10%', title: '姓名',style:'color: #676767;'}
             ,{field:'sex', width:'10%', title: '性别'}
-            ,{field:'city', width:'10%', title: '部门'}
-            ,{field:'sign', title: '入职时间', width: '20%'}
-            ,{field:'score', width:'20%', title: '离职时间'}
+            ,{field:'section', width:'10%', title: '部门'}
+            ,{field:'inTime', title: '入职时间', width: '20%'}
+            ,{field:'outTime', width:'20%', title: '离职时间'}
             ,{field:'classify', width:'12.8%', title: '操作',templet:'#titleTpr'}
           ]]
           ,page: true
-          ,limit: 30
+          ,limit: 10
         });
         //监听表格复选框选择
         table.on('checkbox(demo)', function(obj){
@@ -79,12 +79,12 @@ $(function(){
             // 查看的提示框
             if(obj.event === 'detail'){
                 // layer.msg('ID：'+ data.id + ' 的查看操作');
-                console.log(data)
-                layui.use('layer', function(){ //独立版的layer无需执行这一句
-                    var $ = layui.jquery, layer = layui.layer; //独立版的layer无需执行这一句
-                        layer.open({
-                            type: 1
-                            ,title: ['查看信息','border-bottom: 0 !important;font-weight: bold;font-size:13px;color:#656565;background-color: #ffffff !important;'] //不显示标题栏
+                // console.log(data)
+                layui.use(['layer', 'form'], function(){ //独立版的layer无需执行这一句
+                    var $ = layui.jquery, layer = layui.layer, form = layui.form; //独立版的layer无需执行这一句
+                    layer.open({
+                            type: 2
+                            ,title: ['查看信息'] //不显示标题栏
                             ,closeBtn: 1
                             ,area: ['400px', '500px']
                             ,shade: 0.4
@@ -92,8 +92,7 @@ $(function(){
                             ,btn: ['确定']
                             ,btnAlign: 'c'
                             ,moveType: 1 //拖拽模式，0或者1
-                            // ,content:'../../../html/iframes/company/check-layer.html'
-                            ,content:'<div class="check-panl" style="height:100%;margin:0 20px;"><ul class="person-detail"><li class="person-id" style="height: 30px;line-height: 30px;padding-left: 5px;">ID: <span>'+data.id+'</span></li><li class="person-name" style="height: 30px;line-height: 30px;padding-left: 5px;">姓名： <span>'+data.name+'</span></li><li class="person-sex" style="height: 30px;line-height: 30px;padding-left: 5px;">性别：<span>'+data.sex+'</span></li><li class="person-deplt" style="height: 30px;line-height: 30px;padding-left: 5px;">部门： <span>'+data.city+'</span></li><li class="person-inDate" style="height: 30px;line-height: 30px;padding-left: 5px;">入职时间： <span>'+data.sign+'</span></li><li class="person-outDate" style="height: 30px;line-height: 30px;padding-left: 5px;">离职时间： <span>'+data.score+'</span></li></ul></div>'
+                            ,content:'../../../html/iframes/company/check-layer.html?'+encodeURI(JSON.stringify(data))
                         });            
                 })  
                     
@@ -107,7 +106,31 @@ $(function(){
                 });
                 // 编辑的提示框
             } else if(obj.event === 'edit'){
-                layer.alert('编辑行：<br>'+ JSON.stringify(data))
+                // layer.alert('编辑行：<br>'+ JSON.stringify(data))
+                layui.use(['form','layer','laydate'], function(){ //独立版的layer无需执行这一句
+                    var $ = layui.jquery, form = layui.form, layer = layui.layer, laydate = layui.laydate; //独立版的layer无需执行这一句
+                        laydate.render({
+                            elem: '#maskCalendarIn'
+                            ,lang: 'en'
+                        })
+                        laydate.render({
+                            elem: '#maskCalendarOut'
+                            ,lang: 'en'
+                        })
+                        layer.open({
+                            type: 2 // type为1content是拼接字符串，为2是页面链接
+                            ,title: ['编辑信息'] //不显示标题栏
+                            ,closeBtn: 1
+                            ,area: ['400px', '500px']
+                            ,shade: 0.4
+                            ,id: 'LAY_layuipro7' //设定一个id，防止重复弹出
+                            ,btn: ['确定']
+                            ,btnAlign: 'c'
+                            ,moveType: 1 //拖拽模式，0或者1
+                            ,content:'../../../html/iframes/company/check-layer.html?'+encodeURI(JSON.stringify(data))
+                            // ,content:'<div class="check-panl" style="height:100%;margin:0 20px;"><ul class="person-detail"><li style="display:none;">ID:<span>'+data.id+'</span></li><li><div class="text-box" style="margin-top:10px;"><label class="layui-form-label" style="padding-left:0;text-align:left;line-height:13px;">姓名</label><div class="layui-input-block" style="margin-left:0;"><input type="text" name="title" lay-verify="title" autocomplete="off" placeholder="'+data.name+'" class="layui-input" style="width:160px;height:30px;"></div></div></li><li><div class="text-box" style="margin-top:10px;"><label class="layui-form-label" style="padding-left:0;text-align:left;line-height:13px;">性别</label><div class="layui-input-block" style="margin-left:0;"><input type="text" name="title" lay-verify="title" autocomplete="off" placeholder="'+data.sex+'" class="layui-input" style="width:160px;height:30px;"></div></div></li><li><div class="text-box" style="margin-top:10px;"><label class="layui-form-label" style="padding-left:0;text-align:left;line-height:13px;">部门</label><div class="layui-input-block" style="margin-left:0;"><input type="text" name="title" lay-verify="title" autocomplete="off" placeholder="'+data.section+'" class="layui-input" style="width:160px;height:30px;"></div></div></li><li><div class="text-box" style="margin-top:10px;"><label class="layui-form-label" style="padding-left:0;text-align:left;line-height:13px;">入职时间</label><div class="layui-input-block" style="margin-left:0;"><input type="text" name="title" lay-verify="title" autocomplete="off" placeholder="'+data.inTime+'" class="layui-input" id="maskCalendarIn" style="width:160px;height:30px;"></div></div></li><li><div class="text-box" style="margin-top:10px;"><label class="layui-form-label" style="padding-left:0;text-align:left;line-height:13px;">离职时间</label><div class="layui-input-block" style="margin-left:0;"><input type="text" name="title" lay-verify="title" autocomplete="off" placeholder="'+data.outTime+'" class="layui-input" id="maskCalendarOut" style="width:160px;height:30px;"></div></div></li></ul></div>'
+                        });            
+                })                
             }
         });
         
